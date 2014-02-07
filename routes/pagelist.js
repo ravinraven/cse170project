f18b20e4c2557a03e55211807b0d9906c3d397d0
Kittinan Ponkaew <kponkaew@ucsd.edu>
2014-02-06 17:21:26 -0800

    pulling merge

--- routes/pagelist.js
+++ routes/pagelist.js
@@ -1,13 +1,11 @@
 exports.viewlog = function(req, res) { 
   var name = req.params.name; 
-  console.log("This function from login should be called");
   res.render('login',{});
   // controller code goes here 
 };
 
 exports.viewbeach = function(req, res) { 
   var name = req.params.name; 
-  console.log("This function from beachlist should be called");
   res.render('beachlist',{});
   // controller code goes here 
 };
