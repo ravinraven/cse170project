a26713296706d1eeac6dcec8e9ea45724a1178d2
Kittinan Ponkaew <kponkaew@ucsd.edu>
2014-02-24 21:18:15 -0800

    new weather api yahoo

--- app.js
+++ app.js
@@ -15,6 +15,13 @@ var pagelist = require('./routes/pagelist'); //every page should go to pagelist
 // Example route
 // var user = require('./routes/user');
 
+// Connect to the Mongo database, whether locally or on Heroku
+// MAKE SURE TO CHANGE THE NAME FROM 'lab7' TO ... IN OTHER PROJECTS
+var local_database_name = 'beachbumData';
+var local_database_uri  = 'mongodb://localhost/' + local_database_name
+var database_uri = process.env.MONGOLAB_URI || local_database_uri
+mongoose.connect(database_uri);
+
 var app = express();
 
 // all environments
