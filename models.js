cf9b4b666d8a524766e2d0fb0d73baae74b29245
Kittinan Ponkaew <kponkaew@ucsd.edu>
2014-03-04 21:47:15 -0800

    model

--- models.js
+++ models.js
@@ -3,10 +3,8 @@ var Mongoose = require('mongoose');
 
 
 var ProjectSchema = new Mongoose.Schema({
-	"title":String,"date":String,"summary":String,"image":String
+	"beachname":String,"name":String,"activity":[{"name":String, "count":Number}],"crowd":{"lastupdate":String, "status":String}
   // fields are defined here
 });
 
 exports.Project = Mongoose.model('Project', ProjectSchema);
-
-
